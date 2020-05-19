bd04ac4b722df9b8c77f0808f45b170e6e83a531
blegrand <benjamin.legrand93@gmail.com>
2020-05-19 21:46:56 +0200

    Added basic bottom nav and theme

---
+++ app/src/main/java/fr/legrand/runalytics/presentation/ui/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package fr.legrand.runalytics.presentation.ui.settings
+
+import fr.legrand.runalytics.R
+import fr.legrand.runalytics.presentation.ui.base.BaseFragment
+
+class SettingsFragment : BaseFragment() {
+    override fun getLayoutId(): Int = R.layout.fragment_settings
+}
